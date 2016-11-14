bf734b7d6a4d715e7dbc0f3e679e1b16ac5aaa70
马晨亮 <571950737@qq.com>
2016-11-14 19:47:59 +0800

    新闻+1

--- app/scripts/controllers/insight.js
+++ app/scripts/controllers/insight.js
@@ -10,6 +10,14 @@
 angular.module('yoApp')
   .controller('insight', function () {
   	
+  	$('.new_list').width(($('.new_list li').eq(0).width()+50)*$('.new_list li').length);
+  	$('.new_list').width(($('.new_list li').eq(0).width()+50)*$('.new_list li').length);
+  	var nwe_width= document.body.clientWidth || document.documentElement.clientWidth;
+  	$('.new_list').css({
+  		'top' :  0 + 'px',
+		'left' : -225 + 'px',
+		'position' : 'absolute'
+  	})
 	var mousex = 0, mousey = 0;
     var divLeft, divTop;
     $('.new_list').mousedown(function(e)
@@ -25,6 +33,9 @@ angular.module('yoApp')
 	function dragElement(event)
 	{
 	    var left = divLeft + (event.pageX - mousex);
+	    console.log(event.pageX)
+	    console.log(mousex)
+	    console.log(left);
 	    var top = divTop + (event.pageY - mousey);
 	    $(this).css(
 	    {
@@ -34,8 +45,16 @@ angular.module('yoApp')
 	});
         return false;
     }
-    $(document).mouseup(function()
+    $(document).mouseup(function(event)
     {
+        var new_move1 = event.pageX - mousex;
+        var new_move2=new_move1/360;
+		var new_move=parseInt('new_move2')-230;
+        $('.new_list').css({
+	  		'top' :  0 + 'px',
+			'left' : new_move + 'px',
+			'position' : 'absolute'
+		})
         $('.new_list').unbind('mousemove');
     });
    
