5c45b55678dfc61bff7b888f7f2681442db0e54a
wyy <1778401703@qq.com>
2016-11-15 08:19:10 +0800

    donghua

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,40 +10,14 @@
 
 angular.module('yoApp')
 	.controller('main', function() {
-
-var index = 0;
-/*var autoChange = setInterval(function() {
-	if(index < $(".frame li").length - 1) {
-		index++;
-	} else {
-		index = 0;
-	}	
-	changeTo(index);
-}, 5000);
-$(".frame").find("li").each(function(i) {
-	$(this).click(function() {
-		clearInterval(autoChange);
-		changeTo(i);
-		index = i;
-	}, function() {
-		autoChange = setInterval(function() {
-			if(index < $(".frame li").length - 1) {
-				index++;
-=======
 //		alert(document.documentElement.clientHeight);		
 		var curIndex = 0; //当前index
 		var autoChange = setInterval(function() {
 			if(curIndex < $(".frame li").length - 1) {
 				curIndex++;
->>>>>>> origin/master
 			} else {
 				curIndex = 0;
 			}
-<<<<<<< HEAD
-			changeTo(index);
-		}, 3000);
-	});
-});*/
 			//调用变换处理函数
 			changeTo(curIndex);
 		}, 5000);
@@ -70,7 +44,6 @@ $(".frame").find("li").each(function(i) {
 			$(".frame").find("li").removeClass("imgOn").hide(1000).eq(num).slideDown(1000).addClass("imgOn");
 			$(".frame_btn").find(".afBtn").removeClass("indexOn").eq(num).addClass("indexOn");
 		}
+		clearInterval(autoChange);
 
 	});
-	
-clearInterval(autoChange);
