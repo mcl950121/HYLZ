9d04b11d738bcdad1d5a93a0cf46391bd0071686
abc <463258342@qq.com>
2016-11-18 08:40:04 +0800

    hjm
    11.18 push

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,19 +41,29 @@ angular
 		})
 		$urlRouterProvider.when('', '/main');
 	});
+	
+//$("[id*='Img']").stop(true).on('click', function(e) {
+//	e.preventDefault();
+//	$(this).scrolld();
+//})
 
+//导航切换效果
 
 $('.nav-open1').click(function() {
 	$('.nav-open1').hide();
 	$('.nav-open2').show();
 	$('.nav-content').addClass("nav-active");
+
 })
 $('.nav-open2').click(function() {
 	$('.nav-open1').show();
 	$('.nav-open2').hide();
 	$('.nav-content').removeClass("nav-active");
+
 })
 $('.nav-one').click(function() {
+
+	//nav-home
 	$('.nav-one').removeClass("opa");
 	$('.nav-two').addClass("opa");
 	$('.nav-three').addClass("opa");
@@ -62,6 +72,14 @@ $('.nav-one').click(function() {
 	$('.nav-open1').show();
 	$('.nav-open2').hide();
 	$('.nav-content').removeClass("nav-active");
+   
+    $('#next0Img').removeClass('alike');
+	$('#next4Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
+	$('.home-con').slideDown(2000);
 })
 $('.nav-two').click(function() {
 	$('.nav-two').removeClass("opa");
@@ -72,6 +90,13 @@ $('.nav-two').click(function() {
 	$('.nav-open1').show();
 	$('.nav-open2').hide();
 	$('.nav-content').removeClass("nav-active");
+
+	$('#next1Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('#next4Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
 })
 $('.nav-three').click(function() {
 	$('.nav-three').removeClass("opa");
@@ -82,6 +107,15 @@ $('.nav-three').click(function() {
 	$('.nav-open1').show();
 	$('.nav-open2').hide();
 	$('.nav-content').removeClass("nav-active");
+
+	$('#next2Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('#next4Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
+//	$('.w-box').slideUp();
+	$('.w-box').slideDown(2000);
 })
 $('.nav-fore').click(function() {
 	$('.nav-fore').removeClass("opa");
@@ -92,8 +126,17 @@ $('.nav-fore').click(function() {
 	$('.nav-open1').show();
 	$('.nav-open2').hide();
 	$('.nav-content').removeClass("nav-active");
+
+    $('#next3Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next4Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
+	$('.Big').slideUp();
+	$('.Big').slideDown(2000);
 })
-	
+
 $('.nav-five').click(function() {
 	$('.nav-five').removeClass("opa");
 	$('.nav-one').addClass("opa");
@@ -103,15 +146,108 @@ $('.nav-five').click(function() {
 	$('.nav-open1').show();
 	$('.nav-open2').hide();
 	$('.nav-content').removeClass("nav-active");
+
+    $('#next4Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('.next_bg').addClass('yinC');
+//	$('.new_main').slideUp();
+	$('.new_main').slideDown(2000);
+
 })
 
-$('.logo').click(function(){
-	//alert(1)
+$('.logo').click(function() {
 	alert($('.da').length)
 	$('.da').show;
-	
-	})
 
+})
 
+//底部页面切换效果
+$('#next0Img').click(function() {
+	$('#next1Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('#next4Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
 
+   $('.nav-two').removeClass("opa");
+	$('.nav-one').addClass("opa");
+	$('.nav-three').addClass("opa");
+	$('.nav-fore').addClass("opa");
+	$('.nav-five').addClass("opa");
+	$('.nav-open1').show();
+	$('.nav-open2').hide();
+	$('.nav-content').removeClass("nav-active");
+})
+$('#next1Img').click(function() {
+	$('#next2Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('#next4Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
+    
+    $('.nav-three').removeClass("opa");
+	$('.nav-one').addClass("opa");
+	$('.nav-two').addClass("opa");
+	$('.nav-fore').addClass("opa");
+	$('.nav-five').addClass("opa");
+	$('.nav-open1').show();
+	$('.nav-open2').hide();
+	$('.nav-content').removeClass("nav-active");
+})
+$('#next2Img').click(function() {
+	$('#next3Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next4Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
+ 
+    $('.nav-fore').removeClass("opa");
+	$('.nav-one').addClass("opa");
+	$('.nav-two').addClass("opa");
+	$('.nav-three').addClass("opa");
+	$('.nav-five').addClass("opa");
+	$('.nav-open1').show();
+	$('.nav-open2').hide();
+	$('.nav-content').removeClass("nav-active");
+})
+$('#next3Img').click(function() {
+	$('#next4Img').removeClass('alike');
+	$('#next0Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('.next_bg').addClass('yinC');
 
+    $('.nav-five').removeClass("opa");
+	$('.nav-one').addClass("opa");
+	$('.nav-two').addClass("opa");
+	$('.nav-three').addClass("opa");
+	$('.nav-fore').addClass("opa");
+	$('.nav-open1').show();
+	$('.nav-open2').hide();
+	$('.nav-content').removeClass("nav-active");
+
+})
+$('#next4Img').click(function() {
+	$('#next0Img').removeClass('alike');
+	$('#next4Img').addClass('alike');
+	$('#next1Img').addClass('alike');
+	$('#next2Img').addClass('alike');
+	$('#next3Img').addClass('alike');
+	$('.next_bg').removeClass('yinC');
+
+    $('.nav-one').removeClass("opa");
+	$('.nav-two').addClass("opa");
+	$('.nav-three').addClass("opa");
+	$('.nav-fore').addClass("opa");
+	$('.nav-five').addClass("opa");
+	$('.nav-open1').show();
+	$('.nav-open2').hide();
+	$('.nav-content').removeClass("nav-active");
+})
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,31 +12,24 @@ angular.module('yoApp')
 	.controller('main', function() {
 
 		var curIndex = 0;
-		var aTime;		 
-        
-        aTime = setInterval(function() {
-			if(curIndex < 1) {
-				curIndex++;
-			} else {
-				curIndex = 0;
-			}
-			changeTo(curIndex);
-		}, 5000);
-		
+		var aTime;
+		aTime = setInterval(aChang, 8000);
+		// 左键头
 		$('.fBtn_left').click(function() {
-            
-            clearInterval(aTime);
-			$('.fBtn_left').addClass("indexOn");
-			$('.fBtn_right').removeClass("indexOn");
-			
-			curIndex--;
-			if(curIndex < 0) {
-				curIndex = 1
-			}
-			changeTo(curIndex);
 
-		})
+				clearInterval(aTime);
+				$('.fBtn_left').addClass("indexOn");
+				$('.fBtn_right').removeClass("indexOn");
 
+				curIndex--;
+				if(curIndex < 0) {
+					curIndex = 1
+				}
+				changeTo(curIndex);	
+				
+				aTime = setInterval(aChang, 8000);
+			})
+			// 右键头
 		$('.fBtn_right').click(function() {
 
 			clearInterval(aTime);
@@ -47,55 +40,73 @@ angular.module('yoApp')
 			if(curIndex > 1) {
 				curIndex = 0;
 			}
-			changeTo(curIndex);
+			changeTo(curIndex);	
+			
+			aTime = setInterval(aChang, 8000);
 		})
-		
-		setInterval(aTime);
-		
-		function changeTo(num) {
-			$(".frame").find("li").removeClass("imgOn").hide().eq(num).fadeIn(2000).addClass("imgOn");
+        		
+		function aChang() {
+			if(curIndex < 1) {
+				curIndex++;
+			} else {
+				curIndex = 0;
+			}
+			changeTo(curIndex);
 		}
-		
-        
-               
+
+		function changeTo(num) {
+
+			$(".frame").find("li").removeClass("imgOn").animate({left:'-50px',opacity:0},1000).fadeOut(1000).eq(num).animate({left:'0',opacity:1},50).fadeIn(300).addClass("imgOn");
+            
+		}
+
 		$('.arrow_white1').click(function() {
-			clearInterval(aTime);
 			$('#covered').fadeIn(1000);
+
+			$("#huan").show(); //显示
+			setTimeout(function() { //5秒后隐藏
+				$("#huan").hide();
+			}, 5000);
+            
+            setTimeout(function(){//5秒后显示ʾ
+                $(".covered_con").fadeIn();
+            }, 5000);
+           						
 		})
 
 		$('.delete').click(function() {
-			setInterval(aTime);
+
 			$('#covered').fadeOut(1000);
+
 		})
 
-
 		//自顶替滚动条效果
-		item_btn.onmousedown = function(e) {
-			var ev = e || event;
-			document.onmousemove = function(e) {
-				var ev = e || event;
+		// item_btn.onmousedown = function(e) {
+		// 	var ev = e || event;
+		// 	document.onmousemove = function(e) {
+		// 		var ev = e || event;
 
-				var maxW = item_line.offsetWidth - item_btn.offsetWidth;
-				var maxH = item_line.offsetHeight - item_btn.offsetHeight;
+		// 		var maxW = item_line.offsetWidth - item_btn.offsetWidth;
+		// 		var maxH = item_line.offsetHeight - item_btn.offsetHeight;
 
-				var l = ev.clientX - item_line.offsetLeft - item_btn.offsetWidth / 2;
-				var t = ev.clientY - item_line.offsetTop - item_btn.offsetHeight / 2;
+		// 		var l = ev.clientX - item_line.offsetLeft - item_btn.offsetWidth / 2;
+		// 		var t = ev.clientY - item_line.offsetTop - item_btn.offsetHeight / 2;
 
-				l = l < 0 ? 0 : l;
-				l = l > maxW ? maxW : l;
-				t = t < 0 ? 0 : t;
-				t = t > maxH ? maxH : t;
-				item_btn.style.left = l + 2 + 'px';
-				item_btn.style.top = t + 'px';
+		// 		l = l < 0 ? 0 : l;
+		// 		l = l > maxW ? maxW : l;
+		// 		t = t < 0 ? 0 : t;
+		// 		t = t > maxH ? maxH : t;
+		// 		item_btn.style.left = l + 2 + 'px';
+		// 		item_btn.style.top = t + 'px';
 
-				var a = item_btn.offsetTop / (item_line.offsetHeight - (item_btn.offsetHeight - 2));
-				details_con.style.top = -(details_con.offsetHeight - details.offsetHeight) * a + "px";
-			}
-			
-			document.onmouseup = function() {
-				document.onmousemove = document.onmouseup = null;
-			}
-			return false;
-		}
+		// 		var a = item_btn.offsetTop / (item_line.offsetHeight - (item_btn.offsetHeight - 2));
+		// 		details_con.style.top = -(details_con.offsetHeight - details.offsetHeight) * a + "px";
+		// 	}
+
+		// 	document.onmouseup = function() {
+		// 		document.onmousemove = document.onmouseup = null;
+		// 	}
+		// 	return false;
+		// }
 
 	});
